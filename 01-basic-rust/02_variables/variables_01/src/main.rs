393c3d43ad8c028d6ac882917399932a859a361c
implabinash <implabinash@gmail.com>
2024-08-27 13:56:32 +0530

    Solve variables_01

--- 01-basic-rust/02_variables/variables_01/src/main.rs
+++ 01-basic-rust/02_variables/variables_01/src/main.rs
@@ -1,5 +1,13 @@
 // Swap two variables without using a third variable.
 
+use std::mem::swap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut first_number: u8 = 89;
+    let mut second_number: u8 = 34;
+
+    println!("First variable: {}, Second variable: {}", first_number, second_number);
+    
+    swap(&mut first_number, &mut second_number);
+    println!("First variable: {}, Second variable: {}", first_number, second_number);
 }

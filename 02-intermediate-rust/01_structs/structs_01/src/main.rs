5ce036b6767c037aac2d396a16d8fdf510783ec9
implabinash <implabinash@gmail.com>
2024-08-30 21:13:27 +0530

    Solve structs_01

---
+++ 02-intermediate-rust/01_structs/structs_01/src/main.rs
@@ -0,0 +1,19 @@
+// Create a struct to represent a book with fields for title, author, and pages.
+
+struct Book {
+    title: String,
+    author: String,
+    pages: u32
+}
+
+fn main() {
+    let book = Book {
+        title: "The Rust Programming Language".to_string(),
+        author: "Steve Klabnik and Carol Nichols".to_string(),
+        pages: 552
+    };
+
+    println!("Book Title: {:?}", book.title);
+    println!("Book Author: {:?}", book.author);
+    println!("Book Pages: {:?}", book.pages);
+}

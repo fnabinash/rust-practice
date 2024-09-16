776e71c18ebe3ae60892404b736eb49c4a7f5560
“unpeturbed” <ayodeledrust@gmail.com>
2024-09-16 02:17:36 +0100

    Implemet method for Book struct

--- 02-intermediate-rust/01_structs/structs_02/src/main.rs
+++ 02-intermediate-rust/01_structs/structs_02/src/main.rs
@@ -10,11 +10,20 @@ struct Book {
     pages: u32
 }
 
+//Implemtent a method for the book struct to display its details.
+impl Book {
+    fn display_details(&self) {
+        println!("Title: {}", self.title);
+        println!("Author: {}", self.author);
+        println!("Pages: {}", self.pages);
+    }
+}
+
 fn main() {
     let book: Book = Book {
         title: "Programming Rust".to_string(),
         author: "Jim Blandy, Jason Orendorff and Leonora F.S. Tindall".to_string(),
         pages: 736
     };
-    println!("{book:?}");
+    book.display_details();
 }

92326a40e2f6425e9bc0d266c9477cd9240d7e53
unpeturbed <ayodeledrust@gmail.com>
2024-09-19 11:03:03 +0100

    Print Hello World to Console

--- 01-basic-rust/01_hello_world/hello_world_01/src/main.rs
+++ 01-basic-rust/01_hello_world/hello_world_01/src/main.rs
@@ -1,5 +1,31 @@
-// Print "Hello, World!" to the console.
-
+/// This is a simple "Hello, World!" program in Rust.
+/// 
+/// The main function is the entry point of the program.
+/// 
+/// When executed, it prints "Hello, world!" to the console.
 fn main() {
-    println!("Hello, world!");
+    println!("{}", print_hello());
+}
+
+/// Returns the string "Hello, world!".
+///
+/// # Returns
+///
+/// A `String` containing "Hello, world!"
+fn print_hello() -> String {
+    "Hello, world!".to_string()
+}
+
+// Run`cargo test` to compile and run the tests defined by the #[cfg(test)] attribute.
+#[cfg(test)]
+mod tests {
+    // This brings all items from the parent module into scope,
+    // allowing us to use the `print_hello()` function in our tests.
+    use super::*;
+
+    /// Test to ensure that the `print_hello()` function returns the correct string.
+    #[test]
+    fn test_hello() {
+        assert_eq!(print_hello(), "Hello, world!");
+    }
 }
